73f93bafa017f4500ee47366ce902d8c619b1f00
Saman Ashoori <saman@samanashoori.com>
2025-11-24 23:23:12 +0000

    getting to grips with pointers and refs

--- Sort_Three_Ints.cpp
+++ Sort_Three_Ints.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void swap_by_ref(int &a, int &b) //&a means the function gets passed the memory adress of a so we pass in memory adress
+{
+	//because you cannot change the physical location in memory you have to change the value of the variable
+    int temp = a;
+	//value of a is now b
+    a = b;
+	//value of b is now a
+    b = temp;
+}
+void swap_by_ptr(int *a, int *b) //*a means the function gets passed the pointer
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+	//pointers can be null so you dont have to declare them
+}
+
+void swap_by_ptr(int *a, int *b);
+void swap_by_ref(int &a, int &b);
+
 int main(){
-	int a = 5; //Declaring a value for a
-	int *B;  //Declaring the pointer
+	int x = 5; //Declaring a value for x
+	int y = 2;
+	int z = 11;
+	int *A;  //Declaring the pointer
+	int *B;
+	int *C;
 
-	B = &a;
+	//store memory address of value x in memory address A
+	A = &x;
+	B = &y;
+	C = &z;
 
-	cout << a <<" is the value of the variable a."<<endl;
-        cout << &a << " is the address of a.\n\n";
-	cout << B <<" is the address of the variable.\n";
-	cout << *B <<" is the value of B.\n";
-	
+	cout << x << ' ' << y << '\n';
+    cout << *A <<" is the value of the address A.\n";
+    cout << *B <<" is the value of the address B.\n";
+	cout << "========================================== \n";
+	//Break to make console output easier to see
+
+	swap_by_ref(x,y);
+	//Checks to see if the swaps worked
+	cout << *A <<" is the value of the address A.\n";
+	cout << *B <<" is the value of the address B.\n";
+
 	return 0;
 }
+
