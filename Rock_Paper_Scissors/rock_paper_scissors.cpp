85cd8304ef8c66022c921379fd681b78ec5cf6d3
Saman Ashoori <saman@samanashoori.com>
2025-12-04 22:06:50 +0000

    Refactor code structure for improved readability and maintainability

--- Rock_Paper_Scissors/rock_paper_scissors.cpp
+++ Rock_Paper_Scissors/rock_paper_scissors.cpp
@@ -1,10 +1,23 @@
 #include <iostream> //for character input and output
 #include <random>
+#include <unordered_map>
 
 int main()
 {
+    //Seed Random Number
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    //set max and min for random number
     int max = 3;
     int min = 1;
+
+    // Create an unordered_map
+    std::unordered_map<int,std::string> umap;
+
+    // Insert key-value pairs into the unordered_map
+    umap[1] = "Rock";
+    umap[2] = "Paper"; 
+    umap[3] = "Scissors"; 
 
     std::cout << "Welcome to Rock Paper Scissors - Please 1 for Rock , 2 for Paper and 3 for Scissors: ";
     int x{}; //defining variable to hold user input
@@ -13,7 +26,7 @@ int main()
     int range = max - min + 1;
     int computerChoice = rand() % range + min;
 
-    std::cout << "You entered: " << x << '\n';
-    std::cout << "Computer entered: " << computerChoice << '\n';
+    std::cout << "You entered: " << umap[x] << '\n';
+    std::cout << "Computer entered: " << umap[computerChoice] << '\n';
 
 }
