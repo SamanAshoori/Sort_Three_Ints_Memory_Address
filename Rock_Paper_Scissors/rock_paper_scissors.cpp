b20cb9b45f366e28c4c6494d5200bd0e13f107ef
Saman Ashoori <saman@samanashoori.com>
2025-12-04 22:00:23 +0000

    Add iostream and random includes; implement basic Rock Paper Scissors game logic

--- Rock_Paper_Scissors/rock_paper_scissors.cpp
+++ Rock_Paper_Scissors/rock_paper_scissors.cpp
@@ -1,1 +1,19 @@
-//I will not include iostream because I am a chad_cracked_programmer
+#include <iostream> //for character input and output
+#include <random>
+
+int main()
+{
+    int max = 3;
+    int min = 1;
+
+    std::cout << "Welcome to Rock Paper Scissors - Please 1 for Rock , 2 for Paper and 3 for Scissors: ";
+    int x{}; //defining variable to hold user input
+
+    std::cin >> x; //get number from user
+    int range = max - min + 1;
+    int computerChoice = rand() % range + min;
+
+    std::cout << "You entered: " << x << '\n';
+    std::cout << "Computer entered: " << computerChoice << '\n';
+
+}
