eb02553954b3b1069f983ba64f654047436b8614
Saman Ashoori <saman@samanashoori.com>
2025-12-10 00:02:49 +0000

    Remove unused includes for cleaner code

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -1,10 +1,8 @@
 //Hangman
 #include <iostream> //for character input and output
 #include <cstdlib>
-#include <unordered_map>
 #include <string>
 #include <ctime>
-#include <vector> //for scoring
 
 std::string selectRandomWord();
 
