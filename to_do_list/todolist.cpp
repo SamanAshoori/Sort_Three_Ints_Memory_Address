9d0ae0ae68fa82cb3d4a834e81672dbedcf6c17f
Saman <saman.ashoori@outlook.com>
2026-01-10 23:26:27 +0000

    somethign

--- to_do_list/todolist.cpp
+++ to_do_list/todolist.cpp
@@ -4,33 +4,6 @@
 #include <chrono>
 #include <ctime> // Required for time_t
 
-//Class Definition for task
-class Task {
-public:
-    std::string description;
-    bool completed;
-    std::chrono::system_clock::time_point creationTime;
-    std::chrono::system_clock::time_point dueDate; 
-
-    // Constructors
-    Task(std::string desc);
-    Task(std::string desc, std::chrono::system_clock::time_point due);
-    
-    // Method
-    void markCompleted();
-};
-
-//class construction
-Task::Task(std::string desc) 
-    : description(desc), completed(false), creationTime(std::chrono::system_clock::now()) {}
-
-Task::Task(std::string desc, std::chrono::system_clock::time_point due) 
-    : description(desc), completed(false), creationTime(std::chrono::system_clock::now()), dueDate(due) {}
-
-void Task::markCompleted() {
-    completed = true;
-}
-
 
 //main.cpp
 void writeFile(std::string input); // Function Prototype
@@ -52,29 +25,7 @@ int main(){
     }
     inputFile.close();
 
-    std::cout << "---------------------------" << std::endl;
 
-    // Test the Task class
-    Task task1("Grocery Shopping");
-    std::time_t task1Time = std::chrono::system_clock::to_time_t(task1.creationTime);
-    std::cout << "Task Description: " << task1.description << std::endl;
-    std::cout << "Task Completed: " << task1.completed << std::endl;
-    std::cout << "Task Creation Time: " << std::ctime(&task1Time); // ctime converts time_t to string automatically
-
-    std::cout << "---------------------------" << std::endl;
-
-    Task task2("Pay Bills", std::chrono::system_clock::now() + std::chrono::hours(24));
-    std::time_t task2Time = std::chrono::system_clock::to_time_t(task2.creationTime);
-    std::time_t task2Due = std::chrono::system_clock::to_time_t(task2.dueDate);
-
-    std::cout << "Task Description: " << task2.description << std::endl;
-    std::cout << "Task Completed: " << task2.completed << std::endl;
-    std::cout << "Task Creation Time: " << std::ctime(&task2Time);
-    std::cout << "Task Due Date: " << std::ctime(&task2Due);
-
-    task1.markCompleted();
-    std::cout << "Task1 Completed status updated to: " << task1.completed << std::endl;
-    return 0;
 }
 
 void writeFile(std::string input){
