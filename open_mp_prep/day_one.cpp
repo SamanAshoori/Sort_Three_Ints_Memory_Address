405bbdce231a72cf21ae7bd00063f591e0c58f54
Saman <saman.ashoori@outlook.com>
2026-01-12 23:43:04 +0000

    messing with pointer address to  change variable

--- open_mp_prep/day_one.cpp
+++ open_mp_prep/day_one.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+void doubleValue(int* input);
+
+
+void doubleValue(int* input){
+    *input *= 2;
+}
+
+
+
 int main(){
     int arr[100];
 
@@ -7,5 +16,14 @@ int main(){
         arr[i] = i;
         std::cout << arr[i] << "lives at this address" <<  &arr[i] <<std::endl;
     }
+    std::cout << "Value of arr[16] is: " << arr[16] << std::endl;
+    doubleValue(&arr[16]);
+    
+    std::cout << "Distance in bytes: " << ((char*)&arr[13] - (char*)&arr[0]) << std::endl;
+    std::cout << "Position arr[16] is now: " << arr[16] << std::endl;
+
     return 0;
 }
+
+//So ints are stored as 4 bytes
+//52 Bytes = 4 x 13
